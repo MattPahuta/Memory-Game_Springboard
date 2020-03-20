d8234cd8cc768d039f133b0e6b6b7c4ac01e23ac
Matt Pahuta <mhpahuta@gmail.com>
2020-03-20 14:25:29 -0700

    update and refine modal, cleanup

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 console.log('New Memory Game Scripts connected!')
-// ********** Requirements *******************************************************
-// * Complete * Click .card reveals image - capture each 'click' and add to counter
-// * Complete * Only two images can be visable at any one time
-// * Complete * Clicking on two matching images are a 'match' - images should remain face up
-// * Complete * Non-matching pairs should remain visable for 1500 before hiding again
-// * Complete * Store lowest storing game in local storage - retain 'best score' 
-
 
 const cards = document.querySelectorAll('.memory-card');
 const resetButton = document.querySelector('#reset-btn');
@@ -25,7 +18,6 @@ let lowScore = localStorage.getItem('low-score'); // Track best score in local s
 const modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
-
 
 
 if (lowScore) {
@@ -48,7 +40,6 @@ function flipCard() {
     firstCard = this;
     cardCounter++; // increment counter
     currentScore.textContent = cardCounter; // Display current clicks
-    console.log(cardCounter); // log counter
     return;
   }
     // second click
@@ -56,7 +47,6 @@ function flipCard() {
     secondCard = this;
     cardCounter++; // increment counter
     currentScore.textContent = cardCounter; // Display current clicks
-    console.log(`cardCounter var is: ${cardCounter}`); // log counter
     checkForMatch();
 }
 
@@ -70,21 +60,16 @@ function disableCards() {
     // Card pair matched!
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    console.log('Card pair matched!') // logging
     gameOver++;
-    console.log(`Game Over Counter is: ${gameOver}`); // logging
-    console.log(`Current Low Score is: ${lowScore}`); // logging
-    if (gameOver === 10){ // 10 for 20 -- 8 for 16 card deck (original 6 for 12)
+    if (gameOver === 10){ 
       setTimeout(function() { endGame(); }, 500);
     } 
     resetBoard();
-
 }
 
 function unflipCards() {
   lockBoard = true;
   // Not a match
-  console.log('Card pair not a match.')
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
@@ -99,40 +84,33 @@ function resetBoard () {
   secondCard = null;
 }
 
-
 function endGame() {
   if ((lowScore > currentScore.innerText) || !lowScore) {
-      localStorage.setItem("low-score", currentScore.innerText);
-      bestScore.innerText = currentScore.innerText;
-      alert(`You've got the new best score of ${cardCounter}`);
+    localStorage.setItem("low-score", currentScore.innerText);
+    bestScore.innerText = currentScore.innerText;
+    document.querySelector('h4').innerHTML = 'You solved the game at warp speed! The new top score of <span id="modal-click-counter"></span> is yours!'
+    document.getElementById("modal-click-counter").innerText = cardCounter;
+    modal.style.display = "flex";
   } else{
-    // alert(`You won in ${cardCounter} clicks!`)
     document.getElementById("modal-click-counter").innerText = cardCounter;
     modal.style.display = "flex";
   }
-
 }
-
-
 
 // IIFE (Immediately Invoked Function Expression)
 (function shuffle(){
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 20) // shuffle 12 (original) card deck
+    let randomPos = Math.floor(Math.random() * 20) // shuffle
     card.style.order = randomPos; // randomize based on flexbox property 'order' - assign random order to each card  
   });
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-// Utility *********************
-// localStorage.clear();
-
-
 // *********** Game Winning Modal ******************************** 
-
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+// Utility *********************
+// localStorage.clear();
